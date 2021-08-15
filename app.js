81e3b7e6a1461ccee940f9f6cad4c382c7a576b6
youssefya7 <88986954+youssefya7@users.noreply.github.com>
2021-08-15 23:35:28 +0300

    Add cors-anywhere.herokuapp proxy

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ window.addEventListener('load', ()=> {
       long = position.coords.longitude;
       lat = position.coords.latitude;
       
-      const api = `https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/fd9d9c6418c23d94745b836767721ad1/${lat},${long}`
       
       fetch(api)
       .then(response => {
